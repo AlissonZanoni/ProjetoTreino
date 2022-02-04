5ee7c4c88b20a0cb4daead5c625e01e7d235e024
AlissonZanoni <alisson.zanonii@gmail.com>
2022-02-03 22:41:51 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Linkacesso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnsalvar = new System.Windows.Forms.Button();
+            this.btnacesso = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdesc = new System.Windows.Forms.TextBox();
+            this.txtacesso = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnremover = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Descricao,
+            this.Linkacesso});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 326);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Descricao
+            // 
+            this.Descricao.HeaderText = "Descrição";
+            this.Descricao.Name = "Descricao";
+            this.Descricao.ReadOnly = true;
+            this.Descricao.Width = 200;
+            // 
+            // Linkacesso
+            // 
+            this.Linkacesso.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Linkacesso.HeaderText = "Link do acesso";
+            this.Linkacesso.Name = "Linkacesso";
+            this.Linkacesso.ReadOnly = true;
+            // 
+            // btnsalvar
+            // 
+            this.btnsalvar.Location = new System.Drawing.Point(12, 412);
+            this.btnsalvar.Name = "btnsalvar";
+            this.btnsalvar.Size = new System.Drawing.Size(89, 26);
+            this.btnsalvar.TabIndex = 3;
+            this.btnsalvar.Text = "Salvar";
+            this.btnsalvar.UseVisualStyleBackColor = true;
+            this.btnsalvar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnacesso
+            // 
+            this.btnacesso.Location = new System.Drawing.Point(107, 412);
+            this.btnacesso.Name = "btnacesso";
+            this.btnacesso.Size = new System.Drawing.Size(89, 26);
+            this.btnacesso.TabIndex = 4;
+            this.btnacesso.Text = "acessar";
+            this.btnacesso.UseVisualStyleBackColor = true;
+            this.btnacesso.Click += new System.EventHandler(this.btnacesso_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Descrição";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Link do acesso";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtdesc
+            // 
+            this.txtdesc.Location = new System.Drawing.Point(107, 6);
+            this.txtdesc.Name = "txtdesc";
+            this.txtdesc.Size = new System.Drawing.Size(185, 20);
+            this.txtdesc.TabIndex = 0;
+            // 
+            // txtacesso
+            // 
+            this.txtacesso.Location = new System.Drawing.Point(428, 6);
+            this.txtacesso.Name = "txtacesso";
+            this.txtacesso.Size = new System.Drawing.Size(360, 20);
+            this.txtacesso.TabIndex = 1;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(713, 38);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 2;
+            this.btnadd.Text = "Adicionar";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnremover
+            // 
+            this.btnremover.Location = new System.Drawing.Point(632, 38);
+            this.btnremover.Name = "btnremover";
+            this.btnremover.Size = new System.Drawing.Size(75, 23);
+            this.btnremover.TabIndex = 8;
+            this.btnremover.Text = "Remover";
+            this.btnremover.UseVisualStyleBackColor = true;
+            this.btnremover.Click += new System.EventHandler(this.btnremover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnremover);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtacesso);
+            this.Controls.Add(this.txtdesc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnacesso);
+            this.Controls.Add(this.btnsalvar);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Controle de acessos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnsalvar;
+        private System.Windows.Forms.Button btnacesso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdesc;
+        private System.Windows.Forms.TextBox txtacesso;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnremover;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Linkacesso;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"C:\\I\\acesso.txt");
+            string[] values;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split(';');
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextWriter writer = new StreamWriter(@"C:\\I\\acesso.txt");
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    writer.Write(dataGridView1.Rows[i].Cells[j].Value.ToString()+";");
+                }
+                writer.WriteLine("");
+            }
+            MessageBox.Show("Salvo com sucesso :)", "Sistema informa!");
+            writer.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (txtdesc.Text == "")
+            {
+                MessageBox.Show("Informe a descrição", "Sistema informa!");
+            }
+            else if (txtacesso.Text == "")
+                  {
+                    MessageBox.Show("Informe o link de acesso", "Sistema informa!");
+                  }
+                    else
+                    {
+                    dataGridView1.Rows.Add(txtdesc.Text, txtacesso.Text);
+                    txtacesso.Clear();
+                    txtdesc.Clear();
+                    }
+           
+        }
+
+        private void btnremover_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                try 
+                {
+                    dataGridView1.Rows.RemoveAt(row.Index);
+                }
+                catch
+                {
+                    MessageBox.Show("Selecione uma linha com registros", "Sistema informa!");
+                }
+
+            }
+        }
+
+        private void btnacesso_Click(object sender, EventArgs e)
+        {
+            string endereco;
+            DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+            endereco = selectedRow.Cells[1].Value.ToString();
+            System.Diagnostics.Process.Start(endereco);
+                    
+        }
+    }
+}
